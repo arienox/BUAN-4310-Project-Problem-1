492389c3425006e10d1c820e4920a6d5e65c529e
amikinnamon <92274453+amikinnamon@users.noreply.github.com>
2021-10-10 14:32:50 -0700

    Update Project 1.R

--- Project 1.R
+++ Project 1.R
@@ -4,3 +4,9 @@ Loki <- read.csv("patents_7.csv")
 install.packages("dplyr")
 library(dplyr)
 
+patents_by_country <- Loki %>%
+  group_by(ee_country) %>%
+  summarize(count = n())
+
+patents_by_country <- patents_by_country[with(patents_by_country,order(-count)),]
+patents_by_country[1:10,]
